700f972c101849845b1045dec20ed44ad08ed1b2
zaborch1k <ezaroch13@gmail.com>
2025-03-09 19:09:59 +0300

    all front

--- app.py
+++ app.py
@@ -3,23 +3,83 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-
 @app.route("/dates", methods=["POST", "GET"])
 def dates():
     if request.method == "POST":
-        return "post"
+        date = request.form['date']
+        # data от бд
+        data = {
+            "21.01.2020": {
+                "windows": [
+                    {
+                        1: [True, False, True],
+                        2: [True, False]
+                    },
 
-    return render_template("dates.html")
+                    {
+                        4: [True, False, True],
+                        5: [True, False]
+                    },
+                ],
+
+                "roomCount": 4,
+                "isCorrect": True,
+                "roomNumbers": [1, 2, 3],
+            },
+
+            "21.01.2021": {
+                "windows": [
+                    {
+                        1: [True, False, True],
+                        2: [True, False]
+                    },
+
+                    {
+                        4: [True, False, True],
+                        5: [True, False]
+                    },
+                ],
+
+                "roomCount": 4,
+                "isCorrect": True,
+                "roomNumbers": [1, 2, 3],
+            },
+        }
+        return render_template("windows.html", data=data)
+
+    # dates от бд
+    dates = ["dddd", "dddddddd", "ddddddddddd"]
+    return render_template("dates.html", dates=dates)
 
 
 @app.route("/input", methods=["POST", "GET"])
 def input():
     if request.method == "POST":
-        return "post"
+        roomPerFloor = request.form['roomPerFloor']
+        windowsPerRoom = request.form['windowsPerRoom']
+        lightInfo = request.form['lightInfo']
+
+        # данные в алгоритм
+        data = {
+            "введенные данные": {
+                "windows": [
+                    {
+                        1: [True, False, True],
+                        2: [True, False]
+                    },
+
+                    {
+                        4: [True, False, True],
+                        5: [True, False]
+                    },
+                ],
+
+                "roomCount": 4,
+                "isCorrect": "введенные данные",
+                "roomNumbers": [1, 2, 3],
+            }
+        }
+        return render_template("windows.html", data=data)
 
     return render_template("input.html")
 
